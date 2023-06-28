2e09667b876c393f30a6eea0465d9f21c9c64580
LaurelineAB <laureline.agabibrac@gmail.com>
2023-06-28 10:21:58 +0000

    Opérations principales

--- Calculatrice/index.php
+++ Calculatrice/index.php
@@ -1,5 +1,48 @@
 <?php
 
-    if(issest($_POST["op"]) && !empty($_POST["op"]))
+    require "calculator.phtml";
+    
+    if(isset($_POST["submit"]))
+    {
+        $op = htmlspecialchars($_POST["op"]);
+        $nb1 = htmlspecialchars($_POST["nb1"]);
+        $nb2 = htmlspecialchars($_POST["nb2"]);
+        // var_dump($op, $nb1, $nb2);
+    }
+    
+    require "operations.php";
+    
+    if (isset($op))
+    {
+        if ($op === "add")
+        {
+            $result = add($nb1, $nb2);
+            echo //$result;
+            "<h3>Résultat : $result</h3>";
+        }
+        if ($op === "sub")
+        {
+            $result = substract($nb1, $nb2);
+            echo "<h3>Résultat : $result</h3>";;
+        }
+        if ($op === "mult")
+        {
+            $result = multiply($nb1, $nb2);
+            echo "<h3>Résultat : $result</h3>";;
+        }
+        if ($op === "div")
+        {
+            $result = divide($nb1, $nb2);
+            echo "<h3>Résultat : $result</h3>";;
+        }
+        if ($op === "mod")
+        {
+            $result = modulo($nb1, $nb2);
+            echo "<h3>Résultat : $result</h3>";;
+        }
+        
+    }
+    
+    
 
 ?>
---
+++ Calculatrice/operations.php
@@ -0,0 +1,38 @@
+<?php
+
+    function add (int $nb1, int $nb2) : int
+    {
+        return $nb1 + $nb2;
+    }
+    function substract (int $nb1, int $nb2) : int
+    {
+        return $nb1 - $nb2;
+    }
+    function multiply (int $nb1, int $nb2) : int
+    {
+        return $nb1 * $nb2;
+    }
+    function divide (int $nb1, int $nb2) : ?int
+    {
+        if ($nb2 === 0)
+        {
+            return "On ne peut pas diviser par zéro.";
+        }
+        else
+        {
+            return $nb1 / $nb2;
+        }
+    }
+    function modulo (int $nb1, int $nb2) : ?int
+    {
+        if ($nb2 === 0)
+        {
+            return "On ne peut pas diviser par zéro.";
+        }
+        else
+        {
+            return $nb1 % $nb2;
+        }
+    }
+
+?>
